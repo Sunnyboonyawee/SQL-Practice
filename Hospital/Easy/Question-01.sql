b79cce3c5873d2f04ef847d2ff59d17ab12b2257
Sunshine <145938420+Sunnyboonyawee@users.noreply.github.com>
2025-04-12 18:33:43 +0700

    Update Question-01.sql

--- Hospital/Easy/Question-01.sql
+++ Hospital/Easy/Question-01.sql
@@ -1,4 +1,4 @@
-#Show first name, last name, and gender of patients whose gender is 'M'
+/*Show first name, last name, and gender of patients whose gender is 'M'*/
 SELECT
   first_name,
   last_name,
