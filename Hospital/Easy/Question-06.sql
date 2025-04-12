fe515fa9c0a65d4af0d09a601cbad693600bada0
Sunshine <145938420+Sunnyboonyawee@users.noreply.github.com>
2025-04-12 18:35:10 +0700

    Update Question-06.sql

--- Hospital/Easy/Question-06.sql
+++ Hospital/Easy/Question-06.sql
@@ -1,3 +1,3 @@
-#Show first name and last name concatinated into one column to show their full name.
+/*Show first name and last name concatinated into one column to show their full name.*/
 SELECT concat(first_name,' ',last_name) AS full_name
 FROM patients
